b918f9db189a2b7c218b386a0ff517dc06f4c252
cxmarcelo <marcelo.c.b22@gmail.com>
2022-06-15 18:40:17 -0300

    Zuul timeout

--- hr-worker/src/main/java/com/mcb/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/mcb/hrworker/resources/WorkerResource.java
@@ -37,14 +37,12 @@ public class WorkerResource {
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
 		
 		
-		/*
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		*/
 		
 		logger.info("Port = " + env.getProperty("local.server.port"));
 		
